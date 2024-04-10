407f153e716f93f6bbd98b58d8ed83282a548472
Sawsan Jamal <selhalab297@gmail.com>
2024-04-10 17:57:41 -0500

    final app

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:expense_tracker/models/widgets/expenses.dart';
+import 'package:flutter/material.dart';
+
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 31, 48, 5));
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 9, 20, 0));
+void main() {
+  runApp(
+    MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+              color: kDarkColorScheme.onSecondary,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: kDarkColorScheme.onPrimaryContainer,
+                  foregroundColor: kDarkColorScheme.onInverseSurface)),
+        ),
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.onPrimary),
+          cardTheme: const CardTheme().copyWith(
+              color: kColorScheme.onPrimary,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: kColorScheme.primaryContainer)),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onBackground,
+                  fontSize: 18,
+                ),
+              ),
+        ),
+        home: const Expenses()),
+  );
+}
